ffa51328939bfefa39e20470c725ae688c64c150
fanqiang <fan.qiang_2499@immomo.com>
2019-12-10 20:04:06 +0800

    修改：修改获取用户信息url

--- app/src/main/java/com/cosmos/operatorlogin/Constant.java
+++ app/src/main/java/com/cosmos/operatorlogin/Constant.java
@@ -1,5 +1,5 @@
 package com.cosmos.operatorlogin;
 
 public class Constant {
-    public static final String URL_BASE  = "https://cosmos.immomo.com/admin/oneclick/test/fakeServer";
+    public static final String URL_BASE  = "https://cosmos-web.immomo.com";
 }
