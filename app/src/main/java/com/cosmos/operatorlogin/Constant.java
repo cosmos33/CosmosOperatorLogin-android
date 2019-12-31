019c31d12ca9ce96b6f9393952771ec7f20e6175
fanqiang <fan.qiang_2499@immomo.com>
2019-12-31 18:34:16 +0800

    修改：修改demo URL_BASE以及联通appid,appkey
    (cherry picked from commit 1499dc715da7bbedf3f27bd8d75b0c612c221188)

--- app/src/main/java/com/cosmos/operatorlogin/Constant.java
+++ app/src/main/java/com/cosmos/operatorlogin/Constant.java
@@ -1,5 +1,6 @@
 package com.cosmos.operatorlogin;
 
 public class Constant {
-    public static final String URL_BASE  = "https://cosmos-web.immomo.com";
+    public static final String URL_BASE  = "https://cosmos-web.immomo.com/admin/oneclick/test/fakeServer";
+//    public static final String URL_BASE  = "https://cosmos.wemomo.com";
 }
